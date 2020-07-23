910555e3dcd75539bbd60ab5aa4d9023da31bc62
lkr2 <lokesh.rathinavel@equifax.com>
2020-07-22 22:32:26 -0400

    wordbreak

--- src/main/java/com/example/workout/leetcode/medium/WordBreak_139.java
+++ src/main/java/com/example/workout/leetcode/medium/WordBreak_139.java
@@ -8,20 +8,26 @@ import java.util.Set;
 public class WordBreak_139 {
 
   public static void main(String[] args) {
-    wordBreak("leetcode", Arrays.asList("leet","code"));
+    System.out.println(wordBreak("leetcode", Arrays.asList("leet","co","de")));
   }
 
 
   public static boolean wordBreak(String s, List<String> wordDict) {
-    Set<String> wordDictSet=new HashSet(wordDict);
-    boolean[] dp = new boolean[s.length() + 1];
+
+    Set<String> words = new HashSet<String>(wordDict);
+
+    boolean[] dp = new boolean[s.length()+1];
+
     dp[0] = true;
-    for (int i = 1; i <= s.length(); i++) {
-      for (int j = 0; j < i; j++) {
-        if (dp[j] && wordDictSet.contains(s.substring(j, i))) {
-          dp[i] = true;
+
+    for(int i=1; i<= s.length(); i++){
+      for(int j=0;j<i;j++){
+
+        if(dp[j] && words.contains(s.substring(j, i))){
+          dp[i]=true;
           break;
         }
+
       }
     }
     return dp[s.length()];
