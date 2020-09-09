3d904c3a4334e929dfb431cfb70fa67831eb24ab
lkr2 <lokeshkumar84@gmail.com>
2020-09-09 15:34:09 -0400

    basic calculator II

--- src/main/java/com/example/workout/leetcode/medium/Basic_Calculator_II_227.java
+++ src/main/java/com/example/workout/leetcode/medium/Basic_Calculator_II_227.java
@@ -1,4 +1,10 @@
 package com.example.workout.leetcode.medium;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 /*
 Implement a basic calculator to evaluate a simple expression string.
 
@@ -25,15 +31,65 @@ public class Basic_Calculator_II_227 {
 
     public static void main(String arg[]){
 
-        System.out.println(calculate("3*2*2"));
+        System.out.println(calculate("3-2*2") == 7);
+
 
     }
 
 
     public static int calculate(String s) {
+
+        //s = 3 + 2 * 2;
         int answer = 0;
-        //implementation.
+        List<Integer> numbers = getNumbers(s);
+        List<Character> operators = getOperators(s);
+        Stack<Integer> stack = new Stack<Integer>();
+
+        stack.push(numbers.get(0));
+
+        int num_index = 1, op_index=0;
+
+        while( num_index < numbers.size() && op_index < operators.size()){
+
+            if(operators.get(op_index) == '+'){
+                stack.push(numbers.get(num_index));
+            }else if(operators.get(op_index) == '-'){
+                stack.push(numbers.get(num_index)*-1);
+            }else if(operators.get(op_index) == '*'){
+                stack.push(stack.pop() * numbers.get(num_index));
+            }else if(operators.get(op_index) == '/'){
+                stack.push(stack.pop() / numbers.get(num_index));
+            }
+
+            num_index++;
+            op_index++;
+        }
+
+        while (!stack.empty()){
+            answer += stack.pop();
+        }
         return answer;
+    }
+
+    private static List<Integer> getNumbers(String str){
+
+        List<Integer> numbersList = new ArrayList<>();
+        String[] numbers = str.split("\\+|\\-|\\*|\\/");
+        for(String s:numbers){
+            numbersList.add(Integer.valueOf(s));
+        }
+        return numbersList;
+    }
+
+    private static List<Character> getOperators(String str){
+
+        List<Character> operatorList = new ArrayList<>();
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i) == '+' || str.charAt(i) == '-' || str.charAt(i) == '*' || str.charAt(i) == '/'){
+                operatorList.add(str.charAt(i));
+            }
+        }
+        return operatorList;
     }
 
 }
