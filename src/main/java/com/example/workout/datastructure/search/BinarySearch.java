e359bfb142e98a8b24fc52965cb74413aa34f7ab
lokeshkumarrathinavel <lokeshkumar84@gmail.com>
2020-09-14 22:44:39 -0400

    more algorithm

--- src/main/java/com/example/workout/datastructure/search/BinarySearch.java
+++ src/main/java/com/example/workout/datastructure/search/BinarySearch.java
@@ -1,6 +1,6 @@
 package com.example.workout.datastructure.search;
 
-public class binarySearch {
+public class BinarySearch {
 
     public static void main(String args[]){
         int[] arr = { 1, 2,3,4,5,6,10,20};
